498cdfc67593c3012e9acd29a8d4a3737f7063f2
Aaron Sikes <jingadjanga@gmail.com>
2013-11-22 16:38:23 -0500

    Simplified a bit.
    Shouldn't magically asynchronously be tacking stuff on the object,
    unless we actually take a callback in the constructor.

--- src/svn.js
+++ src/svn.js
@@ -2,15 +2,8 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 
 var SVN = module.exports = function SVN(local, remote){
-	var that = this;
-
 	this.local = path.resolve(local);
 	this.remote = remote;
-
-	if (!this.remote)
-	this.get_remote(function(err, result){
-		that.remote = result;
-	})
 }
 
 SVN.prototype.get_remote = function get_remote(next){
@@ -19,8 +12,4 @@ SVN.prototype.get_remote = function get_remote(next){
 		if (err) return next(err);
 		next(null, result['Repository Root']);
 	})
-}
-
-SVN.prototype.update = function update(revision, next){
-	
 }
