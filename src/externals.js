b627ff17156314b4a1e86ddf233b222f971dfbc1
Asjid Kalam <asjid.kalam@gmail.com>
2020-09-09 17:01:58 +0530

    updated fix

--- src/externals.js
+++ src/externals.js
@@ -40,9 +40,9 @@ module.exports.set = function(options, next){
   options = options || {};
   command = command.concat([this.local ? this.local : "."]);
   command = command.concat(['-F', options.file]);
-  command = command.join(' ');
+  command = command.join(' ').split(' ');
 
-  exec(command, {}, function(error, stdout, stderr){
+  exec(command[0], command.slice(1), {}, function(error, stdout, stderr){
     return next(error);
   })
 };
